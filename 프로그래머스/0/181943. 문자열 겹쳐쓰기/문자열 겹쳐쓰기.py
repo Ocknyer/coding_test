1e0005119e9f33e6847e1a34a9af88d2ff435d1e
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2025-12-17 16:55:17 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,7 +1,6 @@
 def solution(my_string, overwrite_string, s):
     answer = ''
     a = my_string[0:s]
-    #b = my_string[s:s+len(overwrite_string)]
     c = my_string[s+len(overwrite_string):]
     
-    return(f"{a}{overwrite_string}{c}")
+    return a + overwrite_string + c
