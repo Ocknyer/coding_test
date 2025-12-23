c9449c2b81e64a6938cd2eefba8f4cbbdfca06ad
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2025-12-23 21:41:32 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,7 +1,10 @@
 def solution(arr, queries):
     
-    for i in queries:
-        arr[i[0]],arr[i[1]] = arr[i[1]],arr[i[0]]
+    # for i in queries:
+        # arr[i[0]],arr[i[1]] = arr[i[1]],arr[i[0]]
+    
+    for i,j in queries:
+        arr[i],arr[j] = arr[j],arr[i]
     
     return arr
         
