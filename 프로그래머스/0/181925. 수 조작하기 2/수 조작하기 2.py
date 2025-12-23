738021ee87477f9875a07dde0c49e08d4526b48b
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2025-12-23 20:22:35 +0900

    [level 0] Title: 수 조작하기 2, Time: 15.75 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,15 +1,20 @@
 def solution(numLog):
     answer = ''
     
-    for i in range(len(numLog) - 1):
-        log = numLog[i+1] - numLog[i]
-        if log == 1:
-            answer += 'w'
-        elif log == -1:
-            answer += 's'
-        elif log == 10:
-            answer += 'd'
-        elif log == -10:
-            answer += 'a'
+    # for i in range(len(numLog) - 1):
+    #     log = numLog[i+1] - numLog[i]
+    #     if log == 1:
+    #         answer += 'w'
+    #     elif log == -1:
+    #         answer += 's'
+    #     elif log == 10:
+    #         answer += 'd'
+    #     elif log == -10:
+    #         answer += 'a'
+    
+    key = dict(zip([1,-1,10,-10],['w','s','d','a']))
             
+    for i in range(1, len(numLog)):
+        answer += key[numLog[i] - numLog[i - 1]]
+        
     return answer
