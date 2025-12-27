f5f41d75566bdcf3ba5682af3dbae8d31c8c8d33
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2025-12-27 18:30:54 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,2 +1,13 @@
 def solution(binomial):
-    return eval(binomial)
+    # return eval(binomial)
+    
+    a, op, b = binomial.split()
+    
+    a, b = int(a), int(b)
+    
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
