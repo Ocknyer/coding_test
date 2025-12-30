22319c9881405965549514cf0d2e71641fa675ff
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2025-12-30 16:42:50 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,10 +1,11 @@
 def solution(my_string, m, c):
-    answer = ''
+#     answer = ''
     
-    sub = [my_string[i:i+m] for i in range(0, len(my_string), m)]
+#     sub = [my_string[i:i+m] for i in range(0, len(my_string), m)]
     
-    for i in sub:
-        answer += i[c-1]
+#     for i in sub:
+#         answer += i[c-1]
         
-    return answer
-        
+#     return answer
+
+    return my_string[c-1::m]    
