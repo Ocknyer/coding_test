c6f702a313a598c6050fd9187e7168a5f86d127f
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2025-12-30 17:11:54 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.25 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,12 +1,23 @@
 def solution(my_string):
-    result = [0 for i in range(52)]
-    alpha = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
+#     result = [0 for i in range(52)]
+#     alpha = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
     
-    for i in my_string:
-        for j, v in enumerate(alpha):
-            if i == v:
-                result[j] += 1
+#     for i in my_string:
+#         for j, v in enumerate(alpha):
+#             if i == v:
+#                 result[j] += 1
                 
+#     return result
+
+    result=[0]*52
+    
+    for x in my_string:
+        if x.isupper():
+            result[ord(x)-65]+=1
+        else:
+            result[ord(x)-71]+=1
+            
     return result
+
     
     
